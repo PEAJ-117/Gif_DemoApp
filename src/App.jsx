ebfdb82338d255d9ed233ec15522a67229c8f77d
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2023-05-18 23:18:14 -0600

    Settings components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import { useState } from "react"
+import Categories from "./components/Categories"
 
 function App() {
-  const [categories, setCategories] = useState(["Dragon Ball","One Piece"])
+  const [categories, setCategories] = useState(["Dragon Ball","One Piece","Spiderman","Fast&Furious"])
 
   return (
-    <>
-      <div className="ms-5 mt-5 me-5">
-        <h3>GIF DEMO APP</h3>
-        <hr />
-        <input type="text" placeholder="Search term" />
-        <button type="button" className="btn btn-outline-primary btn-sm ms-2">Search</button>
-        <br />
-        {
-          categories.map((category) => (
-            <li key={category}>
-              {category}
-            </li>
-          ))
-        }
-      </div>
-    </>
+    <div className="ms-5 mt-5 me-5">
+      <h3>GIF DEMO APP</h3>
+      <hr />
+      <Categories categories={categories}
+                  setCategories={setCategories} />
+      <hr />
+    </div>
   )
 }
 
